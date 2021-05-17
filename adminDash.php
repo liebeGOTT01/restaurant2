f75925fadd98876e89d7cba45bff5e2c9740ecc9
@chloe868 <cherrymae.herrera@student.passerellesnumeriques.org>
2021-05-17 18:38:51 +0800

    some changes in styles

--- adminDash.php
+++ adminDash.php
@@ -1,5 +1,5 @@
                 <?php
-                include('includes/header.php');
+                include('includes/adminHeader.php');
 
                 ?>
 
@@ -16,7 +16,7 @@
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                                 Today Sales</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $object->Get_total_today_sales(); ?></div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
@@ -25,90 +25,7 @@
                                 </div>
                             </div>
                         </div>
-
-                        <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Yesterday Sales</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $object->Get_total_yesterday_sales(); ?></div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Last 7 Day Sales
-                                            </div>
-                                            <div class="row no-gutters align-items-center">
-                                                <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $object->Get_last_seven_day_total_sales(); ?></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Pending Requests Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                All Time Sales</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $object->Get_total_sales();?></div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>                    
-
-                    <?php
-                    
-                    ?>
-                        <div class="col-md-12">
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3">
-                                    <div class="row">
-                                        <div class="col">
-                                            <h6 class="m-0 font-weight-bold text-primary">Live Table Status</h6>
-                                        </div>
-                                        <div class="col" align="right">
-                                            
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="card-body">
-                                    <div id="table_status"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                <?php
-                //include('footer.php');
-                ?>
+                        
 
 <script>
 
