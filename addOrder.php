55fdb7f2d36c510cc6885a38d50aecaf0880aeee
ravenTorres <raven.torres44@gmail.com>
2021-05-19 19:22:26 +0800

    change add order functionalities

--- addOrder.php
+++ addOrder.php
@@ -1,4 +1,6 @@
 <?php 
+$tableNo=$_GET['id'];
+// echo '<script>alert("'.$tableNo.'")</script>';
     include('includes/waiterHeader.php'); 
     include('includes/function.php');
     $myfunction=new functions; 
@@ -7,36 +9,29 @@
 ?>
 
 <div class="container-fluid">
+<!-- <form action="" method="Post"> -->
 	<div class="col-lg-12">
 		<div class="card">
 			<div class="card-header">
-				<?php 
-					$connection =$myfunction->openConnection(); 
-					$statement=$connection->prepare("SELECT table_name FROM table_data");
-					$statement->execute();
-					$table_list = $statement->fetchAll();
-					foreach($table_list as $table) {
-				?>
-					<h4><?php echo $table['table_name']?></h4>
-				<?php
-					}
-				?>
+
+					<h4 name="tableNo" id="tableNo"><?php echo $tableNo?></h4>
+
 			</div>
 			<div class="card-body">
-				<form method="POST">
+
 					<div class="col-md-12">
 						<div class="row">
 							<div class="form-group col-md-5">
 								<label class="control-label">Customer</label>
-                                <input type="text" class="form-control" placeholder="Enter Customer Name">
+                                <input type="text" id="customer" class="form-control" placeholder="Enter Customer Name">
 							</div>
 						</div>
 						<hr>
 						<div class="row mb-3">
 								<div class="col-md-4">
 									<label class="control-label">Product</label>
-									<select name="productOrder" class="form-control" required data-parsley-trigger="change">
-									<option value="">Select Product</option>
+									<select name="productOrder" id="productSelect" class="form-control" required data-parsley-trigger="change">
+									<option value="" >Select Product</option>
                                     <?php
 										$myfunction->getProduct();
 									?>
@@ -51,6 +46,7 @@
 									<button class="btn btn-block btn-sm btn-primary" type="submit" name="addOrder" id="add_order"><i class="fa fa-plus"></i>Add to List</button>
 								</div>
 						</div>
+
 					</form>
 						<div class="row">
 							<table class="table table-bordered" id="list">
@@ -72,28 +68,49 @@
 										<th class="text-right"></th>
 									</tr>
 								</thead>
-								<tbody>
+								<tbody id="tableBody">
 								<?php
-									$i = 1;
-									$connection =$myfunction->openConnection(); 
-									$statement=$connection->prepare("SELECT * FROM order_item_table");
-									$statement->execute();
-									$product_order = $statement->fetchAll();
-									foreach($product_order as $order){
+									// $i = 1;
+									// $connection =$myfunction->openConnection(); 
+									// $statement=$connection->prepare("SELECT * FROM order_item_table");
+									// $statement->execute();
+									// $product_order = $statement->fetchAll();
+									// foreach($product_order as $order){
 								?>
-									<tr>
-										<td class="text-center"><?php echo $i++ ?></td>
-										<td class="text-right">
-											<?php echo $order['product_name'] ?>
+								<script>
+									$(document).ready(function(){
+						$count=1;
+						$('#add-order').click(()=>{
+							$customer=$('#customer').val()
+							console.log($customer)
+							console.log("clicked")
+																							
+							$productName=$('#productSelect').val()
+									
+							console.log($price);
+											
+							$quantity=$('#qty').val();
+							console.log($quantity)
+							$('#tbody').append('<tr><td class="text-center id">'+$count+'</td><td class="text-right quantity">'+$productName+'</td><td class="text-right price">'+$quantity+'</td></tr>')
+							$count++;	
+						})
+					})
+								</script>	
+									<!-- <tr>
+										<td class="text-center id"><?php// echo $i++ ?>
+									
+									</td>
+										<td class="text-right product">
+											<?php //echo $order['product_name'] ?>
 										</td>
-										<td class="text-right">
-											<input type="number" min="1" value="1"class="form-control text-right" step="any" id="qty" value="<?php echo $order['product_quantity'] ?>">
+										<td class="text-right quantity">
+											<!-- <input type="number" min="1" value="1"class="form-control text-right" step="any" id="qty" value="<?php echo $order['product_quantity'] ?>"> -->
+										<!-- </td>
+										<td class="text-right price">
+											<?php //echo "Php ".$order['product_price'] ?>
 										</td>
-										<td class="text-right">
-											<?php echo "Php ".$order['product_price'] ?>
-										</td>
-										<td class="text-right">
-											<?php echo $order['product_quantity']*$order['product_price'] ?>
+										<td class="text-right amount">
+											<?php //echo $order['product_quantity']*$order['product_price'] ?>
 										</td>
 										<td class="text-center">
 											<form action="" method="POST">
@@ -102,10 +119,10 @@
 													<i class="fa fa-trash text-danger" aria-hidden="true" data-toggle="tooltip" data-placement="left" title="Delete Product"></i>
 												</button>
 											</form>
-										</td>
-									</tr>
+										</td> -->
+									</tr> -->
 								<?php 
-									}
+									//}
 								?>
 								</tbody>
 								<tfoot>
@@ -120,8 +137,6 @@
 						<div class="row">
 							<button class="btn btn-primary btn-sm btn-block float-right " type="button" id="pay">Submit to Kitchen for Confirmation</button>
 						</div>
-
-						<h1>need pa ug ajax kada change sa quantity</h1>
 					</div>
 				
 			</div>
--- waiterProduct.php
+++ waiterProduct.php
@@ -5,7 +5,8 @@
 ?>
 
 <div class="container">
-    <div class="col">
+	<div class="row">
+    <div class="col-6">
     <?php 
 		$connection = $myfunction->openConnection();
 		$statement = $connection->prepare("SELECT * FROM product_table ORDER BY category_name ASC");
@@ -14,18 +15,125 @@
 		$productCount = $statement->rowCount();
 		foreach($product as $newProduct) {
 	?>
-		<div class="card" style="width: 18rem;">
+		<div class="card mb-4" style="width:10rem;">
 			<img class="card-img-top" src="<?php echo $newProduct['product_image'] ?>" alt="Card image cap">
-			<div class="card-body">
-				<h5 class="card-title"><?php echo $newProduct['product_name'] ?></h5>
+			<div class="card-body ">
+				<h5 class="card-title prod_name" ><?php echo $newProduct['product_name'] ?></h5>
 				<p class="card-text">
-					<?php echo $newProduct['category_name'] ?>
-					<?php echo $newProduct['product_price'] ?>
+					<span>
+						<?php echo $newProduct['category_name'] ?>
+					</span>
+					<span class="prod_price">
+						<?php echo $newProduct['product_price'] ?>
+					</span>
 				</p>
+			</div>
+			<div class="card-footer">
+				<button type="submit" class="btn btn-success add-order" >Add Order</button>
 			</div>
         </div>
     <?php
         }
     ?>
     </div>
+	<div class="col-6">
+	<form action="" method="POST">
+		<div class="col">
+			<label class="control-label">List of Tables</label>
+			<select name="tableOrder" id="tableSelect" class="form-control" required data-parsley-trigger="change">
+				<option value="" >Select Table</option>
+				<?php
+					$myfunction->getTableName();
+				?>
+            </select>
+		</div>
+		<br>
+		<br>
+		<div class="col">
+			<div class="panel panel-default p-2">
+				<div class="panel-body">table name goes here..</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<table class="table table-bordered" id="list">
+					<colgroup>
+						<col width="5%">
+						<col width="30%">
+						<col width="10%">
+						<col width="25%">
+						<col width="25%">
+						<col width="5%">
+					</colgroup>
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Menu</th>
+							<th>Quantity</th>
+							<th>Price</th>
+							<th>Amount</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody id="tbody">
+
+					</tbody>
+					<tfoot>
+						<tr>
+							<th class="text-right" colspan="4">Total</th>
+							<th class="text-right tamount"></th>
+							<th></th>
+						</tr>
+					</tfoot>
+				</table>
+				<button type="submit" class="btn btn-primary w-100">Submit to kitchen</button>
+				<script>
+					$(document).ready(function(){
+						$count=1;
+						$('.add-order').click(function(){	
+
+							$productName=$(this).parent().prev().children('.prod_name').html()
+							console.log("clicked")
+							$quantity=$('.qty').val();
+							$price= $(this).parent().prev().children('.card-text').children('.prod_price').html()
+							
+							console.log($productName);
+							console.log($price);
+							$('#tbody').append('<tr class="tableRow">'+
+								'<td>'+$count+'</td>'+
+								'<td>'+$productName+'</td>'+
+								'<td>'+'<input type="number" min="1" value="1" class="w-100 quantity">'+'</td>'+
+								'<td class="price">'+$price+'</td>'+
+								'<td class="amount">'+'</td>'+
+								'<td>'+'<button type="submit" class="btn btn-danger text-white btn-circle delete">'+'X'+'</button>'+'</td>'+
+							'</tr>');
+							$count++;	
+							
+							$('.quantity').click(function(){
+								$quantity=$('.quantity').val()
+								console.log($('.quantity').val())
+								$('.quantity').keyup(function(){
+									$quantity=$(this).val()
+									console.log($quantity)
+									$(this).parent().siblings('.amount').html($quantity*$(this).parent().siblings('.price').html())
+								})
+								$quantity=$(this).val()
+								$(this).parent().siblings('.amount').html($quantity*$(this).parent().siblings('.price').html())
+							})
+
+							$(".delete").click(function() {
+								$(this).parent().parent('.tableRow').remove();
+							});
+					
+						})
+						$('#tableSelect').change(function(){
+							$('.panel-body').html($('#tableSelect').val())
+						})
+					})
+				</script>	
+			</div>
+		</div>
+	</form>
+	</div>
+	</div>
 </div>
